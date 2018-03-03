f8c840627bf0dd8e84c738d1798df2f594b3efdc
Zhu Shengqi <zetasq@gmail.com>
2018-03-04 00:08:45 +0800

    Initial Commit

---
+++ SwiftOperation/SwiftOperation.h
@@ -0,0 +1,19 @@
+//
+//  SwiftOperation.h
+//  SwiftOperation
+//
+//  Created by Zhu Shengqi on 04/03/2018.
+//  Copyright © 2018 Zhu Shengqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftOperation.
+FOUNDATION_EXPORT double SwiftOperationVersionNumber;
+
+//! Project version string for SwiftOperation.
+FOUNDATION_EXPORT const unsigned char SwiftOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftOperation/PublicHeader.h>
+
+
